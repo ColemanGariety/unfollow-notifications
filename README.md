5b75923978e077fb40a3cd5c36445f259d5ed6f9
Jackson Gariety <personal@jacksongariety.com>
2013-10-09 16:17:13 -0700

    Re-factors to use Promises/A+ and stores handle in config.json.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ jesus
 
 The son of god lets you know when you get unfollowed on Twitter.
 
+Also a cool example of how to use Promises A+.
+
 ## Installation
 
 Clone this resiprocity.
@@ -10,6 +12,6 @@ Clone this resiprocity.
 ## Setup
 
 1. Create an app at developer.twitter.com
-2. Generate 4 tokens for `config.json` (make sure you change 'read-only' to 'read-write-dm' beforehand)
-3. Put 4 tokens in `config.json`
-4. `node jesus.js` (or `forever start jesus.js` if you're cool)
+2. `cd jesus` and `npm install`
+3. Drop 4 api tokens and a twitter handle into `config.json` (make sure you change 'read-only' to 'read-write-dm' before generating the tokens)
+4. Run `node jesus.js` *(or `forever start jesus.js` if you're one of the cool kids)*
--- jesus.js
+++ jesus.js
@@ -1,78 +1,78 @@
-function ask(question, callback) {
-  var stdin = process.stdin
-    , stdout = process.stdout
-
-  stdin.resume();
-  stdout.write(question + ": ");
-
-  stdin.once('data', function(data) {
-    data = data.toString().trim();
-
-    callback(data)
-  });
-}
-
-function filter (collection, iterator) {
-  if (Array.isArray(collection)) {
-    var newCollection = new Array()
-    for (var i = 0; i < collection.length; i++) iterator(collection[i]) && newCollection.push(collection[i])
-  } else {
-    var newCollection = new Object()
-    for (var i in collection) if (iterator(collection[i])) newCollection[i] = collection[i]
-  }
-    
-  return newCollection
-}
-
-function contains (obj, target) {
-  if (obj == null) return false
-  return obj.indexOf(target) != -1
-}
-
-ask("Your Twitter handle", function(res){
-  var bird = res
-    , followers = []
-    , Twitter = require('twit')
-    , twitter = new Twitter(require('./config.json'))
-
+!function () {
+  // Config
+  var config = require('./config.json')
+  
+  // Classes
+  var Twitter = require('twit')
+    , Promise = require("bluebird")
+  
+  // Instances
+  var twitter = new Twitter(config.twitter_keys)
+    , followerCache = new Array
+  
+  // Methods
   function poll () {
-    twitter.get('followers/ids', function (err, res) {
+    getFollowersOf(config.handle)
+      .then(getScreenNamesOf)
+      .then(send)
+    
+    return poll
+  }
+
+  function getFollowersOf(handle) {
+    var resolver = Promise.pending()
+    
+    twitter.get('followers/ids', function (err, followers) {
       if (err) throw err
-      
-      var unfollowers = filter(followers, function (follower) {
-            return !contains(res.ids, follower)
-          })
-        , i = unfollowers.length
-        , message = "You've been unfollowed by"
-      
-      if (i) {
-        while (i--) {
-          twitter.get('users/show', { user_id: unfollowers[i] }, function (err, unfollower) {
-            if (err) throw err
-            if (unfollowers.length)
-            message += (" @" + unfollower.screen_name)
-            
-            if (i < 0) {
-              message += "."
-              
-              twitter.post('direct_messages/new', { screen_name: bird, text: message }, function (err, res) {
-                if (err) throw err
-                
-                console.log("Sent unfollow notification")
-              })
-            } else {
-              message += ","
-            }
-          })
+  
+      var unfollows = findUnfollows(followerCache, followers)
+  
+      followerCache = followers.ids
+  
+      resolver.fulfill(unfollows)
+    })
+    
+    return resolver.promise
+  }
+
+  function findUnfollows(followerCache, followers) {
+    var unfollows = []
+    
+    for (var i = followerCache.length; i--;) followers.ids.indexOf(followerCache[i]) === -1 && unfollows.push(followerCache[i])
+    
+    return unfollows
+  }
+  
+  function getScreenNamesOf(unfollows) {
+    var resolver = Promise.pending()
+      , message = "You've been unfollowed by"
+    
+    for (var i = unfollows.length; i--;) {
+      twitter.get('users/show', { user_id: unfollows[i] }, function (err, unfollower) {
+        if (err) throw err
+        
+        message += (" @" + unfollower.screen_name)
+        
+        if (i < 0) {
+          message += "."
+          resolver.fulfill(message);
+        } else {
+          message += ","
         }
-      }
-      
-      followers = res.ids
+      })
+    }
+    
+    return resolver.promise
+  }
+  
+  function send(message) {
+    twitter.post('direct_messages/new', { screen_name: config.handle, text: message }, function (err, res) {
+      if (err) throw err
+    
+      console.log(message)
     })
   }
   
-  poll()
-  setInterval(poll, 30000)
-  
-  console.log("Okay, " + res + ", your savior (the son of god) will let you know when someone unfollows you!")
-})
+  // Start
+  setInterval(poll(), 30000)  
+}()
